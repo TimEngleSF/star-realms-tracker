a64a3893503a720c31c25508feb9ca09cdef150b
Tim Engle <timothy.englew@gmail.com>
2024-02-20 00:44:31 -0800

    Add Game struct and reset method

--- cmd/game.go
+++ cmd/game.go
@@ -1,5 +1,16 @@
 package main
 
+import "time"
+
+type Game struct {
+	Players  Players
+	Current  *Player
+	Winner   *Player
+	Loser    *Player
+	Complete bool
+	Date     *time.Time
+}
+
 type Player struct {
 	Id        int
 	Name      string
@@ -10,4 +21,21 @@ type Players []Player
 
 func (ps *Players) AddPlayer(p Player) {
 	*ps = append(*ps, p)
+}
+
+func (g *Game) SwitchCurrentPlayer() {
+	if g.Current == &g.Players[0] {
+		g.Current = &g.Players[1]
+	} else {
+		g.Current = &g.Players[0]
+	}
+}
+
+func (g *Game) Restart() {
+	g.Players = Players{}
+	g.Current = nil
+	g.Winner = nil
+	g.Loser = nil
+	g.Complete = false
+	g.Date = nil
 }
