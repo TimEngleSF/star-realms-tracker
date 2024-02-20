829a31128ec7dbfd5c6b2976e5233332725f15c9
Tim Engle <timothy.englew@gmail.com>
2024-02-20 12:27:35 -0800

    add tailwind autosort prettier plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
   ]
 }
 
