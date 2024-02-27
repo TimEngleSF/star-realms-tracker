0e7c8f3ac56738d9f798260a942db6f5f5eb93f5
Tim Engle <timothy.englew@gmail.com>
2024-02-26 21:04:25 -0800

    Move logic for handling score to 'HandleUpdateScore' in handlers package from main

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -77,6 +77,7 @@ func HandleSelectFirstPlayer(ins *game.InstanceState) echo.HandlerFunc {
 func HandleUpdateCurrPlayer(ins *game.InstanceState) echo.HandlerFunc {
 	g := ins.Game
 	return func(c echo.Context) error {
+		g.Players.ResetAuthorityDifference()
 		for i := range g.Players {
 			p := &g.Players[i]
 			p.IsCurrent = !p.IsCurrent
@@ -85,5 +86,45 @@ func HandleUpdateCurrPlayer(ins *game.InstanceState) echo.HandlerFunc {
 			}
 		}
 		return render(c, http.StatusContinue, views.ScoreboardTemplate(*g))
+	}
+}
+
+func HandleUpdateScore(ins *game.InstanceState) echo.HandlerFunc {
+	g := ins.Game
+
+	return func(c echo.Context) error {
+		id, err := strconv.Atoi(c.QueryParam("player"))
+		scoreAction := c.QueryParam("action")
+
+		if err != nil {
+			log.Printf("Error parsing id for PUT '/score': %v\n", err)
+			c.Response().Header().Set(
+				"HX-Trigger",
+				`{"error": {"id": "scoreboard-error-msg", "message":  "Error updating score: Invalid player ID"}}`,
+			)
+			return render(c, http.StatusInternalServerError, views.ScoreboardTemplate(*g))
+		}
+
+		player := &g.Players[id]
+		score := &player.Authority
+
+		if scoreAction == "add" {
+			player.IncrementAuthority()
+		} else if scoreAction == "subtract" {
+			player.DecrementAuthority()
+		}
+
+		if *score == 0 {
+			g.Loser = player
+			winnerId := 0
+			if id == 0 {
+				winnerId = 1
+			}
+			g.Winner = &g.Players[winnerId]
+			g.Complete = true
+			return render(c, http.StatusOK, views.WinnerTemplate(*g))
+		}
+
+		return render(c, http.StatusOK, views.ScoreboardTemplate(*g))
 	}
 }
